142d39226ce98581a689bff8dfbce3dd878ee452
KitchenLab2 <KitchenLab2@TEKCENTER.LOCAL>
2019-07-23 11:13:26 -0700

    added conn.php

---
+++ conn.php
@@ -0,0 +1,11 @@
+<?php
+
+//Connection File
+$host = 'localhost';
+$username = 'DoomKitty87';
+$pass = '$200^Carrot';
+$dbname = 'website';
+$dsn = "mysql:host=$host;dbname=$dbname";
+$options = array(
+    PDO::ATTR_ERRMODE
+)
