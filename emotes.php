628453d63df6b90ac92bf35e1db80f03a5f009db
KitchenLab2 <KitchenLab2@TEKCENTER.LOCAL>
2019-07-24 15:27:39 -0700

    added emotes

--- emotes.php
+++ emotes.php
@@ -14,7 +14,37 @@ include "conn.php";
 </head>
 <body>
     <div class="container">
-        
+        <h1 class="text-info">Emote Store</h1>
+        <br>
+        <br>
+        <div class="card">
+            <div class="card-body">
+                <br>
+                <div class="row">
+                    <h1 class="text-warning">Smiley Happy - 10 Points</h1>
+                    
+                    <?php
+                    if ($ownedone = "yes") { ?>
+                        <button class="btn btn-danger mr-2 ml-2"><h4>Owned</h4></button>
+                        <?php
+                    }
+                    else { ?>
+                    <button class="btn btn-success mr-2 ml-2"><h4>Purchase</h4></button>
+<?php
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
     </div>
+    <script>
+        function myFunction14() {
+            <?php
+            $id = $_SESSION['id'];
+            $sql = "UPDATE users SET points = points-10 WHERE id = $id";
+            $conn->exec($sql);
+            $ownedone = "yes";
+            ?>
+        }
 </body>
 </html>
