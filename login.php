55b004e813fe13788ce6b103daa7370553c6045b
KitchenLab2 <KitchenLab2@TEKCENTER.LOCAL>
2019-07-24 09:13:01 -0700

    dsadsa

--- login.php
+++ login.php
@@ -1,28 +1,33 @@
 <?php
 include 'conn.php';
 //Check if user has submitted the form
-if (isset($_POST['register'])) {
+if (isset($_POST['login'])) {
     $username = $_POST['username'];
-    $email = $_POST['email'];
     $password = $_POST['password'];
-    $password2 = $_POST['password2'];
-    if ($password == $password2) {
-        $password = password_hash($password, PASSWORD_DEFAULT);
-
-            //Insert into database
-    $sql = "INSERT INTO users (username, email, `password`) VALUES ('$username','$email','$password')";
-    $conn->exec($sql);
-    header('Location: login.php');
-    die;
+    $sql = "SELECT * FROM users WHERE username = '$username'";
+    $s = $conn->prepare($sql);
+    $s->execute();
+    $user = $s->fetch(PDO::FETCH_ASSOC);
+    if ($user['username'] == $username) {
+        if (password_verify($password, $user['password'])) {
+            session_start();
+            $_SESSION['username'] = $username;
+            $_SESSION['loggedin'] = 'true';
+            header('Location: home.php');
+            die;
+        }
+        else {
+            echo "<div class='alert alert-danger' role='alert'>
+            Incorrect Password.
+            </div>";
+        } 
     }
     else {
         echo "<div class='alert alert-danger' role='alert'>
-        Passwords Do Not Match.
-      </div>";
+        Invalid Username.
+        </div>";
     }
-
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,7 +35,7 @@ if (isset($_POST['register'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register Your Account</title>
+    <title>Login</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
@@ -40,7 +45,7 @@ if (isset($_POST['register'])) {
             <div class="col-5">
                 <br>
                 <br>
-        <h1 class="text-info" >Register</h1>
+        <h1 class="text-info" >Login</h1>
         <form action="" method="post">
             <br>
             <div class="form-root">
@@ -49,21 +54,11 @@ if (isset($_POST['register'])) {
             </div>
             <br>
             <div class="form-root">
-                <label for="email"><h4 class="text-info mr-1">Enter Email</h4></label>
-                <input type="email" name="email" id="email" required>
-            </div>
-            <br>
-            <div class="form-root">
                 <label for="password"><h4 class="text-info mr-1">Enter Password</h4></label>
                 <input type="password" name="password" id="password" required>
             </div>
             <br>
-            <div class="form-root">
-                <label for="password2"><h4 class="text-info mr-1">Confirm Password</h4></label>
-                <input type="password" name="password2" id="password2" required>
-            </div>
-            <br>
-            <button type="submit" class="btn btn-info" name="register"><h1>Register</h1></button>
+            <button type="submit" class="btn btn-info" name="login"><h1>Login</h1></button>
         </form>
             </div>
         </div>
