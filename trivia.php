6c0ca8b7c83cfa719467e539e91781b84f3f6d8c
DoomKitty87 <doomkitty887@gmail.com>
2019-07-24 17:19:02 -0700

    added questionssss

--- trivia.php
+++ trivia.php
@@ -2,7 +2,7 @@
 include "sess.php";
 include "nav.php";
 include "conn.php";
-$random = rand(1,5);
+$random = rand(1,10);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -68,10 +68,10 @@ $random = rand(1,5);
         }
         ?>
         <?php
-        if ($random == 3) { ?>
+        if ($random == 4) { ?>
         <div class="card">
             <div class="card-body">
-                <h2 class="text-warning">What Format Involves A 99-Card Deck?</h2>
+                <h2 class="text-secondary">What Format Involves A 99-Card Deck?</h2>
                 <br>
                 <div class="row">
                 <button class="btn btn-success mr-2 ml-2" onclick="myFunction12()">Commander</button>
@@ -85,16 +85,100 @@ $random = rand(1,5);
         }
         ?>
         <?php
-        if ($random == 3) { ?>
+        if ($random == 5) { ?>
         <div class="card">
             <div class="card-body">
-                <h2 class="text-warning">What Format Involves Only Cards From The 4-8 Most Recent Sets?</h2>
+                <h2 class="text-danger">What Format Involves Only Cards From The 4-8 Most Recent Sets?</h2>
                 <br>
                 <div class="row">
                 <button class="btn btn-success mr-2 ml-2" onclick="myFunction13()">Modern</button>
     <button class="btn btn-primary mr-2" onclick="myFunction13()">Commander</button>
     <button class="btn btn-danger mr-2" onclick="myFunction13()">Legacy</button>
     <button class="btn btn-info mr-2" onclick="myFunction12()">Standard</button>
+                </div>
+            </div>
+        </div>
+        <?php
+        }
+        ?>
+        <?php
+        if ($random == 6) { ?>
+        <div class="card">
+            <div class="card-body">
+                <h2 class="text-primary">What Was The First MTG "Set"?</h2>
+                <br>
+                <div class="row">
+                <button class="btn btn-success mr-2 ml-2" onclick="myFunction12()">Alpha</button>
+    <button class="btn btn-primary mr-2" onclick="myFunction13()">Legends</button>
+    <button class="btn btn-danger mr-2" onclick="myFunction13()">Kamigawa</button>
+    <button class="btn btn-info mr-2" onclick="myFunction13()">Rath</button>
+                </div>
+            </div>
+        </div>
+        <?php
+        }
+        ?>
+        <?php
+        if ($random == 7) { ?>
+        <div class="card">
+            <div class="card-body">
+                <h2 class="text-warning">Who Invented Magic The Gathering?</h2>
+                <br>
+                <div class="row">
+                <button class="btn btn-success mr-2 ml-2" onclick="myFunction13()">Brian Kibler</button>
+    <button class="btn btn-primary mr-2" onclick="myFunction12()">Richard Garfield</button>
+    <button class="btn btn-danger mr-2" onclick="myFunction13()">Melissa DeTora</button>
+    <button class="btn btn-info mr-2" onclick="myFunction13()">Ben Stark</button>
+                </div>
+            </div>
+        </div>
+        <?php
+        }
+        ?>
+        <?php
+        if ($random == 8) { ?>
+        <div class="card">
+            <div class="card-body">
+                <h2 class="text-secondary">What Was The Most Recent "Unset"?</h2>
+                <br>
+                <div class="row">
+                <button class="btn btn-success mr-2 ml-2" onclick="myFunction13()">Unhinged</button>
+    <button class="btn btn-primary mr-2" onclick="myFunction12()">Unstable</button>
+    <button class="btn btn-danger mr-2" onclick="myFunction13()">Unglued</button>
+                </div>
+            </div>
+        </div>
+        <?php
+        }
+        ?>
+        <?php
+        if ($random == 9) { ?>
+        <div class="card">
+            <div class="card-body">
+                <h2 class="text-warning">What Revised Set Came Out In 2018?</h2>
+                <br>
+                <div class="row">
+                <button class="btn btn-success mr-2 ml-2" onclick="myFunction13()">Ixalan</button>
+    <button class="btn btn-primary mr-2" onclick="myFunction13()">Guilds Of Ravnica</button>
+    <button class="btn btn-danger mr-2" onclick="myFunction13()">Dominaria</button>
+    <button class="btn btn-info mr-2" onclick="myFunction12()">Battle For Zendikar</button>
+                </div>
+            </div>
+        </div>
+        <?php
+        }
+        ?>
+        <?php
+        if ($random == 10) { ?>
+        <div class="card">
+            <div class="card-body">
+                <h2 class="text-warning">What Format Includes A 60 - Card Deck With A Planeswalker And Signature Spell?</h2>
+                <br>
+                <div class="row">
+                <button class="btn btn-success mr-2 ml-2" onclick="myFunction13()">Commander</button>
+    <button class="btn btn-primary mr-2" onclick="myFunction13()">Modern</button>
+    <button class="btn btn-danger mr-2" onclick="myFunction12()">Oathbreaker</button>
+    <button class="btn btn-info mr-2" onclick="myFunction13()">Standard</button>
                 </div>
             </div>
         </div>
